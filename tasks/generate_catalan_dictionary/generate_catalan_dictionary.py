3fe086dc75d7f43aa83ca509f3bedb3852bc6afc
Aitor <aitirga@gmail.com>
2022-02-06 20:00:49 +0100

    feat: upload dictionary

--- tasks/generate_catalan_dictionary/generate_catalan_dictionary.py
+++ tasks/generate_catalan_dictionary/generate_catalan_dictionary.py
@@ -1,25 +1,19 @@
 from spylls.hunspell import Dictionary
+import pandas as pd
+import unidecode
 
 if __name__ == '__main__':
     target_length = 5
     list_of_words = []
-    sample_word = 'anar'
 
+    dictionary = Dictionary.from_files('/Users/aitoriraolagalarza/Desktop/pycharm_projects/wordle_dictionary_builder/data/hunspell-cat/catalan')
 
-    h = Dictionary.from_files('/Users/aitoriraolagalarza/Desktop/pycharm_projects/wordle_dictionary_builder/data/hunspell-cat/catalan')
-    # print(h.dic.words[10000].stem)
-    # print(h.lookup('anava'))
-    list = h.lookuper.produce_affix_forms('menjar')
-
-    for element in list:
-        print(element)
-
-    # dictionary_words = h.dic.words
-    # for word in dictionary_words:
-    #     print(word.flags)
-    #     # if len(word.stem) == target_length:
-    #     #     list_of_words.append(word.stem)
-    #
-    # print(list_of_words)
+    for word in dictionary.dic.words:
+        if str(word.captype) != 'Type.NO': # Don't include words with no capital letters
+            continue
+        if len(word.stem) == target_length:
+            list_of_words.append(unidecode.unidecode(word.stem))
+    df = pd.DataFrame(list_of_words)
+    df.to_csv('catalan_dictionary.csv', index=False, header=False)
 
 
